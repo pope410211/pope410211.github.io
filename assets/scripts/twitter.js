b0301913f60dedc608a5b69b9c0e88d831154821
Nick Pope <pope410211@gmail.com>
2015-09-17 15:34:31 -0400

    twitter added - jquery used for adding actual html

--- assets/scripts/twitter.js
+++ assets/scripts/twitter.js
@@ -8,3 +8,19 @@
     fjs.parentNode.insertBefore(js, fjs);
   }
 }(document, "script", "twitter-wjs");
+
+
+
+if ($(window).width() < 600 ) {
+
+   $('.twitter').removeClass('twitter').addClass('twitter-mobile');
+               $twitterMobile = $('.twitter-mobile')
+               $twitterMobile.append('<a class="twitter-timeline" href="https://twitter.com/pope410211" data-widget-id="644569435425107973" data-tweet-limit="3"   data-chrome="nofooter noheader">Tweets by @pope410211</a>');
+
+}
+else {
+
+   $twitter = $('.twitter')
+   $twitter.append('<a class="twitter-timeline" href="https://twitter.com/pope410211" data-widget-id="644569435425107973"  data-tweet-limit="5">Tweets by @pope410211</a>');
+
+}
